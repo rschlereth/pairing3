ec75c9075416f6023986b5b9f8323aa342dd524f
Robert Schlereth <robert.schlereth@gmail.com>
2022-12-11 13:31:55 -0600

    add another for statement

--- friends.js
+++ friends.js
@@ -4,3 +4,9 @@ for (var i = 0; i < numbers.length; i++) {
         console.log(numbers[i])
     }
 }
+
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
+        console.log(numbers[i])
+    }
+}
